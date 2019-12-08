0fa63fd02629f271f20f0d615a4721440f561c51
Dominic Chu <dominic.m.chu@gmail.com>
2019-12-08 17:23:37 -0500

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-const testVar = {}
 
-function testFunc() {
-  return "hi"
+
+function superbowlWin(record) {
+  let object1 = record.find( 
+    function(o) {
+      return o["result"] === "W"
+    }
+  );
+  if (object1) {
+    return object1["year"]
+  }
 }
